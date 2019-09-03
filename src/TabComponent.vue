1aa1e1366d88af7a4e7247101dab8d6f1d3e4e42
unknown <glendycovarrubias@hotmail.com>
2019-09-03 18:37:27 -0500

    Component skeleton description section

--- src/TabComponent.vue
+++ src/TabComponent.vue
@@ -2,41 +2,38 @@
   <a-tabs defaultActiveKey="2">
     <a-tab-pane key="1">
       <span slot="tab">
-        <a-icon type="apple" />
-        Tab 1
+        <a-icon type="dashboard" />
+        Calificando
       </span>
+      <span>Ranking: </span>
       <rate-component />
       <comment-component />
     </a-tab-pane>
     <a-tab-pane key="2">
       <span slot="tab">
-        <a-icon type="android" />
-        Tab 2
+        <a-icon type="read" />
+        Descripción
       </span>
-      Tab 2
+      <skeleton-component />
     </a-tab-pane>
   </a-tabs>
 </template>
 <script>
 import RateComponent from "./RateComponent.vue"
 import CommentComponent from "./CommentComponent.vue"
+import SkeletonComponent from "./SkeletonComponent.vue"
 
 export default {
   components : {
     RateComponent,
-    CommentComponent
+    CommentComponent,
+    SkeletonComponent
   },
   data() {
     return {
-    //   collapsed     : false,
-    //   titleBar      : "Capacitación",
-    //   titleSection  : "Mi Dashboard </>"
     }
   },
   mounted(){
-    // invocar los métodos
-    // this.getSession();
-    // console.log(this.collapsed);
   },
   methods : {
   }
